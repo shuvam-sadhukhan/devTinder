a6a23b2ac80782bcab0f60029c5033c3d8919f60
shuvam-sadhukhan <sadhukhanshuvam63@gmail.com>
2025-12-30 20:16:37 +0530

    adding files

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -8,7 +8,7 @@ const userAuth=async(req,res,next)=>{
     const cookies=req.cookies;
     const {token}=cookies;  
     if(!token){
-        throw new Error("Token is not valid");  
+      return res.status(401).send('Please login');
     }
     const decodedMessage= await jwt.verify(token,"DEV@Tinder$790");
     const {_id}=decodedMessage;
